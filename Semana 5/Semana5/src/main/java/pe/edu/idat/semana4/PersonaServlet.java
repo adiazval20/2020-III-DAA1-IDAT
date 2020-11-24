8057b5e16e173b0acdcecc2cfe2eff27b9f2b735
Andy <adiazval20@gmail.com>
2020-11-23 19:53:49 -0500

    1

--- Semana 5/Semana5/src/main/java/pe/edu/idat/semana4/PersonaServlet.java
+++ Semana 5/Semana5/src/main/java/pe/edu/idat/semana4/PersonaServlet.java
@@ -3,6 +3,8 @@ package pe.edu.idat.semana4;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,36 +17,97 @@ import pe.edu.idat.semana4.entity.Persona;
 public class PersonaServlet extends HttpServlet {
 
     private List<Persona> personas = new ArrayList<>();
+    private Persona persona = new Persona();
 
     @Override
     public void init() throws ServletException {
         personas.add(new Persona(1, "Diaz", "Valdiviezo", "Andy", "01/01/1990"));
-        personas.add(new Persona(1, "Perez", "Sanchez", "Maria", "01/01/1995"));
+        personas.add(new Persona(2, "Perez", "Sanchez", "Maria", "01/01/1995"));
 
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        persona = new Persona();
+
+        if (req.getParameter("id") != null) {
+            final int personaId = Integer.parseInt(req.getParameter("id"));
+
+            Optional<Persona> optPersona = personas.stream()
+                    .filter(p -> p.getId() == personaId)
+                    .reduce((p, v) -> {
+                        throw new IllegalStateException("No se ha encontrado un solo elemento");
+                    });
+            if (optPersona.isPresent()) {
+                persona = optPersona.get();
+            }
+        }
+
         String apellidoPaterno = req.getParameter("apellidoPaterno");
         String apellidoMaterno = req.getParameter("apellidoMaterno");
         String nombres = req.getParameter("nombres");
         String fechaNacimiento = req.getParameter("fechaNacimiento");
-        int id = personas.size() + 1;
         
-        Persona persona = new Persona(id, apellidoPaterno, apellidoMaterno, nombres, fechaNacimiento);
-        this.personas.add(persona);
+        persona.setApellidoPaterno(apellidoPaterno);
+        persona.setApellidoMaterno(apellidoMaterno);
+        persona.setNombres(nombres);
+        persona.setFechaNacimiento(fechaNacimiento);
         
-        req.setAttribute("personas", this.personas);
-        RequestDispatcher rd = req.getRequestDispatcher("persona.jsp");
-        rd.include(req, resp);
+        if(persona.getId() == 0) {
+            persona.setId(personas.size() + 1);
+            personas.add(persona);
+        } else {
+            personas.stream().forEach(p -> {
+                if(p.getId() == persona.getId()) {
+                    p = persona;
+                }
+            });
+        }
+        
+        persona = new Persona();
+
+        forwardPage(req, resp, "persona.jsp");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("personas", this.personas);
+        int id;
+        persona = new Persona();
 
-        RequestDispatcher rd = req.getRequestDispatcher("persona.jsp");
-        rd.include(req, resp);
+        String accion = req.getParameter("accion");
+        accion = (accion == null ? "" : accion);
+
+        switch (accion) {
+            case "editar":
+                id = Integer.parseInt(req.getParameter("id"));
+                /*persona = personas.stream()
+                        .filter(p -> p.getId() == id)
+                        .collect(Collectors.toList()).get(0);*/
+
+                Optional<Persona> optPersona = personas.stream()
+                        .filter(p -> p.getId() == id)
+                        .reduce((p, v) -> {
+                            throw new IllegalStateException("No se ha encontrado un solo elemento");
+                        });
+                if (optPersona.isPresent()) {
+                    persona = optPersona.get();
+                }
+
+                break;
+            case "eliminar":
+                break;
+        }
+
+        forwardPage(req, resp, "persona.jsp");
     }
 
+    private void forwardPage(HttpServletRequest req, HttpServletResponse resp, String page) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        req.setAttribute("personas", this.personas);
+        req.setAttribute("persona", this.persona);
+
+        RequestDispatcher rd = req.getRequestDispatcher(page);
+        rd.forward(req, resp);
+    }
 }
--- Semana 5/Semana5/target/Semana5-0.1/persona.jsp
+++ Semana 5/Semana5/target/Semana5-0.1/persona.jsp
@@ -16,33 +16,34 @@
             <h2>Mantenimiento de Personas</h2>
             <br>
             <form action="persona" method="POST">
+                <input type="hidden" id="hddId" name="id" value="${persona.getId()}"/>
                 <div class="row form-group">
                     <label for="txtApellidoPaterno" class="col-form-label col-sm-3">Apellido Paterno:</label>
                     <div class="col-sm-6">
-                        <input type="text" name="apellidoPaterno" id="txtApellidoPaterno" class="form-control">
+                        <input type="text" name="apellidoPaterno" id="txtApellidoPaterno" class="form-control" value="${persona.apellidoPaterno}">
                     </div>
                 </div>
                 <div class="row form-group">
                     <label for="txtApellidoMaterno" class="col-form-label col-sm-3">Apellido Materno:</label>
                     <div class="col-sm-6">
-                        <input type="text" name="apellidoMaterno" id="txtApellidoMaterno" class="form-control">
+                        <input type="text" name="apellidoMaterno" id="txtApellidoMaterno" class="form-control" value="${persona.apellidoPaterno}">
                     </div>
                 </div>
                 <div class="row form-group">
                     <label for="txtNombres" class="col-form-label col-sm-3">Nombres:</label>
                     <div class="col-sm-8">
-                        <input type="text" name="nombres" id="txtNombres" class="form-control">
+                        <input type="text" name="nombres" id="txtNombres" class="form-control" value="${persona.nombres}">
                     </div>
                 </div>
                 <div class="row form-group">
                     <label for="txtFechaNacimiento" class="col-form-label col-sm-3">Fecha de Nacimiento:</label>
                     <div class="col-sm-4">
-                        <input type="text" name="fechaNacimiento" id="txtFechaNacimiento" class="form-control">
+                        <input type="text" name="fechaNacimiento" id="txtFechaNacimiento" class="form-control" value="${persona.fechaNacimiento}">
                     </div>
                 </div>
                 <div class="row form-group">
                     <div class="col-sm-9 offset-sm-3">
-                        <button type="submit" class="btn btn-primary">Registrar</button>
+                        <button type="submit" class="btn btn-primary">Guardar</button>
                     </div>
                 </div>
             </form>
@@ -72,6 +73,10 @@
                                         <td><%=persona.getApellidoMaterno() %></td>
                                         <td><%=persona.getNombres() %></td>
                                         <td><%=persona.getFechaNacimiento() %></td> 
+                                        <td>
+                                            <a href="${pageContext.request.contextPath}/persona?accion=editar&id=<%=persona.getId()%>" class="btn btn-sm btn-primary">Editar</a>
+                                            <a href="${pageContext.request.contextPath}/persona?accion=eliminar&id=<%=persona.getId()%>" class="btn btn-sm btn-danger">Eliminar</a>
+                                        </td>
                                     </tr>
                                     <%
                                 }
