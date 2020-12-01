110b9644ff12783810abcb8ae3808b038ecfbe0d
Andy <adiazval20@gmail.com>
2020-11-30 19:26:19 -0500

    1

---
+++ Semana 7/Semana7/src/main/java/pe/edu/idat/semana4/UsuarioServlet.java
@@ -0,0 +1,23 @@
+package pe.edu.idat.semana4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "UsuarioServlet", urlPatterns = "/usuario")
+public class UsuarioServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        
+        String json = "{\"prop1\": \"1\", \"prop2\": \"2\"}";
+        PrintWriter pw = resp.getWriter();
+        pw.println(json);
+    }
+}
