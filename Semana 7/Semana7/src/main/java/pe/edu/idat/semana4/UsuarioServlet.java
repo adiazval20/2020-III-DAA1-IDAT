e67465a93b8d165ba3c58c9f5b67de55f260a483
Andy <adiazval20@gmail.com>
2020-11-30 21:12:12 -0500

    4

--- Semana 7/Semana7/src/main/java/pe/edu/idat/semana4/UsuarioServlet.java
+++ Semana 7/Semana7/src/main/java/pe/edu/idat/semana4/UsuarioServlet.java
@@ -19,6 +19,7 @@ public class UsuarioServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
 
         Gson gson = new Gson();
         
