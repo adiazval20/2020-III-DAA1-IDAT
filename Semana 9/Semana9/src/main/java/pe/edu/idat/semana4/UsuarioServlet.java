6973d9aebaba34d608d781d3702a86e9ba912d89
Andy <adiazval20@gmail.com>
2020-12-14 20:07:03 -0500

    0

---
+++ Semana 9/Semana9/src/main/java/pe/edu/idat/semana4/UsuarioServlet.java
@@ -0,0 +1,72 @@
+package pe.edu.idat.semana4;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pe.edu.idat.semana4.entity.Persona;
+import pe.edu.idat.semana4.entity.Usuario;
+import pe.edu.idat.semana4.repository.UsuarioRepository;
+
+@WebServlet(name = "UsuarioServlet", urlPatterns = "/usuario")
+public class UsuarioServlet extends HttpServlet {
+
+    private UsuarioRepository repo;
+
+    @Override
+    public void init() throws ServletException {
+        repo = UsuarioRepository.getInstance();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        configResponse(resp);
+
+        int id = Integer.parseInt(req.getParameter("id"));
+        Usuario usuario = repo.find(id);
+        
+        String apellidoPaterno = req.getParameter("apellidoPaterno");
+        String apellidoMaterno = req.getParameter("apellidoMaterno");
+        String nombres = req.getParameter("nombres");
+        String fechaNacimiento = req.getParameter("fechaNacimiento");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        
+        usuario.setApellidoPaterno(apellidoPaterno);
+        usuario.setApellidoMaterno(apellidoMaterno);
+        usuario.setNombres(nombres);
+        usuario.setFechaNacimiento(fechaNacimiento);
+        usuario.setUsername(username);
+        usuario.setPassword(password);
+        
+        usuario = repo.save(usuario);
+        
+        Gson gson = new Gson();
+        String json = gson.toJson(usuario);
+        PrintWriter pw = resp.getWriter();
+        pw.println(json);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        configResponse(resp);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(repo.list());
+        PrintWriter pw = resp.getWriter();
+        pw.println(json);
+    }
+
+    private void configResponse(HttpServletResponse resp) {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    }
+}
