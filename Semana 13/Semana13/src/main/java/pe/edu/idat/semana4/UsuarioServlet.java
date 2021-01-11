fe462a6ff87a0c593f94e9dd5f9cdc20a4bb7d9c
Andy <adiazval20@gmail.com>
2021-01-11 18:21:03 -0500

    init

---
+++ Semana 13/Semana13/src/main/java/pe/edu/idat/semana4/UsuarioServlet.java
@@ -0,0 +1,126 @@
+package pe.edu.idat.semana4;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pe.edu.idat.semana4.dao.DbConnection;
+import pe.edu.idat.semana4.dao.UsuarioDao;
+import pe.edu.idat.semana4.entity.Persona;
+import pe.edu.idat.semana4.entity.Usuario;
+import pe.edu.idat.semana4.repository.UsuarioRepository;
+
+@WebServlet(name = "UsuarioServlet", urlPatterns = "/usuario")
+public class UsuarioServlet extends HttpServlet {
+
+    private UsuarioRepository repo;
+    private UsuarioDao dao;
+
+    @Override
+    public void init() throws ServletException {
+        repo = UsuarioRepository.getInstance();
+        dao = UsuarioDao.getInstance();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        configResponse(resp);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("rpta", 1);
+        response.put("msg", "ok");
+
+        int id = Integer.parseInt(req.getParameter("id"));
+        String apellidoPaterno = req.getParameter("apellidoPaterno");
+        String apellidoMaterno = req.getParameter("apellidoMaterno");
+        String nombres = req.getParameter("nombres");
+        String fechaNacimiento = req.getParameter("fechaNacimiento");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        Usuario usuario;
+        try {
+            usuario = dao.find(id);
+            usuario.setApellidoPaterno(apellidoPaterno);
+            usuario.setApellidoMaterno(apellidoMaterno);
+            usuario.setNombres(nombres);
+            usuario.setFechaNacimiento(fechaNacimiento);
+            usuario.setUsername(username);
+            usuario.setPassword(password);
+            usuario = dao.save(usuario);
+            response.put("data", usuario);
+        } catch (SQLException ex) {
+            response.put("msg", ex.getMessage());
+        }
+
+        Gson gson = new Gson();
+        String json = gson.toJson(response);
+        PrintWriter pw = resp.getWriter();
+        pw.println(json);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        configResponse(resp);
+
+        Gson gson = new Gson();
+        Map<String, Object> response = new HashMap<>();
+        response.put("rpta", 1);
+        response.put("msg", "ok");
+
+        try {
+            if (req.getParameter("id") != null) {
+                int id = Integer.parseInt(req.getParameter("id"));
+                Usuario usuario = dao.find(id);
+
+                String accion = req.getParameter("accion");
+                if (accion == null) {
+                    accion = "";
+                }
+
+                if (accion.equals("eliminar")) {
+                    boolean responseDelete = dao.delete(usuario);
+                    response.put("data", responseDelete);
+                    if (responseDelete) {
+                        response.put("msg", "Usuario eliminado correctamente");
+                    } else {
+                        response.put("msg", "No se pudo eliminar el usuario");
+                    }
+                } else {
+                    
+                    response.put("data", usuario);
+                }
+
+            } else {
+                response.put("data", dao.listSp());
+            }
+        } catch (SQLException ex) {
+            response.put("rpta", -1);
+            response.put("msg", ex.getMessage());
+        }
+
+        String json = gson.toJson(response);
+        PrintWriter pw = resp.getWriter();
+        pw.println(json);
+    }
+
+    private void configResponse(HttpServletResponse resp) {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        resp.addHeader("Access-Control-Allow-Headers", "Content-Type");
+    }
+}
---
+++ Semana 13/Semana13/src/main/java/pe/edu/idat/semana4/repository/UsuarioRepository.java
@@ -0,0 +1,53 @@
+package pe.edu.idat.semana4.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import pe.edu.idat.semana4.entity.Usuario;
+
+public class UsuarioRepository {
+
+    private List<Usuario> usuarios;
+    private static UsuarioRepository instance;
+
+    private UsuarioRepository() {
+        usuarios = new ArrayList<>();
+        usuarios.add(new Usuario(1, "Perez", "Rojas", "Miguel", "01/01/2000", "jrojas", "123456"));
+        usuarios.add(new Usuario(2, "Díaz", "Cabrejos", "Alejandra", "01/01/1998", "dcabrejos", "admin"));
+    }
+
+    public static UsuarioRepository getInstance() {
+        if (instance == null) {
+            instance = new UsuarioRepository();
+        }
+        return instance;
+    }
+
+    public List<Usuario> list() {
+        return usuarios;
+    }
+
+    public Usuario find(int id) {
+        Optional<Usuario> optUsuario = usuarios.stream()
+                .filter(u -> u.getPersonaId() == id)
+                .reduce((u, v) -> {
+                    throw new IllegalStateException("Se ha encontrado más de un elemento");
+                });
+        return (optUsuario.isPresent() ? optUsuario.get() : new Usuario());
+    }
+
+    public Usuario save(Usuario usuario) {
+        if (usuario.getPersonaId() == 0) {
+            usuario.setPersonaId(usuarios.size() + 1);
+            usuarios.add(usuario);
+        } else {
+            usuarios.stream()
+                    .forEach(u -> {
+                        if (u.getPersonaId() == usuario.getPersonaId()) {
+                            u = usuario;
+                        }
+                    });
+        }
+        return usuario;
+    }
+}
